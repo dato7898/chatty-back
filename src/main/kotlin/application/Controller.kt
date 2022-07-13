b98d816f8bb7e3aca52392b79ec1291a4a9c2d3f
david <davidd7598@gmail.com>
2022-07-13 17:29:00 +0600

    added http request

--- src/main/kotlin/application/Controller.kt
+++ src/main/kotlin/application/Controller.kt
@@ -1,13 +1,20 @@
 package application
 
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
 
 @RestController
-class Controller {
+class Controller(
+    restTemplateBuilder: RestTemplateBuilder
+) {
+
+    private val restTemplate = restTemplateBuilder.build()
+
     @RequestMapping("/api/greet")
-    fun helloWorld(@RequestParam name: String): String {
-        return "hello $name"
+    fun helloWorld(): String? {
+        return restTemplate.getForObject(URI("https://jsonplaceholder.typicode.com/posts"), String::class.java)
     }
+
 }
