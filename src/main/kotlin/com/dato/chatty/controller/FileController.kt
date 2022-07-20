ad51d4c72b2dec96d15a73e21f633bedc8124121
david <davidd7598@gmail.com>
2022-07-20 17:18:36 +0600

    added google drive api

---
+++ src/main/kotlin/com/dato/chatty/controller/FileController.kt
@@ -0,0 +1,39 @@
+package com.dato.chatty.controller
+
+import com.dato.chatty.google.drive.GoogleDriveService
+import org.apache.commons.io.IOUtils
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import java.io.ByteArrayInputStream
+import java.io.File
+import javax.servlet.http.HttpServletResponse
+
+@RestController
+@RequestMapping("file")
+class FileController(
+    private val googleDriveService: GoogleDriveService
+) {
+
+    @PostMapping("upload")
+    fun uploadFile(@RequestParam("file") file: MultipartFile) {
+        googleDriveService.uploadFile(file.originalFilename, file.contentType, file.bytes)
+    }
+
+    @GetMapping("download/{realFileId}")
+    fun downloadFile(
+        @PathVariable realFileId: String,
+        response: HttpServletResponse
+    ) {
+        println(File("./").absolutePath)
+        val baos = googleDriveService.downloadFile(realFileId)
+        val content = baos.toByteArray()
+        val bais = ByteArrayInputStream(content)
+        IOUtils.copy(bais, response.outputStream)
+        response.flushBuffer()
+    }
+}
---
+++ src/main/kotlin/com/dato/chatty/google/drive/GoogleDriveService.kt
@@ -0,0 +1,62 @@
+package com.dato.chatty.google.drive
+
+import com.google.api.client.googleapis.json.GoogleJsonResponseException
+import com.google.api.client.http.ByteArrayContent
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.gson.GsonFactory
+import com.google.api.services.drive.Drive
+import com.google.api.services.drive.DriveScopes
+import com.google.api.services.drive.model.File
+import com.google.auth.http.HttpCredentialsAdapter
+import com.google.auth.oauth2.GoogleCredentials
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import java.io.ByteArrayOutputStream
+
+@Service
+class GoogleDriveService {
+
+    @Value("\${app.google.drive.parent}")
+    private val googleDriveParent = ""
+
+    private val service: Drive
+
+    init {
+        val credentials = GoogleCredentials.getApplicationDefault().createScoped(listOf(DriveScopes.DRIVE_FILE))
+        val requestInitializer = HttpCredentialsAdapter(credentials)
+        service = Drive.Builder(NetHttpTransport(),
+            GsonFactory.getDefaultInstance(),
+            requestInitializer)
+            .setApplicationName("Drive samples")
+            .build()
+    }
+
+    fun uploadFile(fileName: String?, fileType: String?, fileContent: ByteArray): String {
+        val fileMetadata = File()
+        fileMetadata.name = fileName
+        fileMetadata.parents = listOf(googleDriveParent)
+        val mediaContent = ByteArrayContent(fileType, fileContent)
+        try {
+            val file = service.files().create(fileMetadata, mediaContent)
+                .setFields("id")
+                .execute()
+            return file.id
+        } catch (e: GoogleJsonResponseException) {
+            System.err.println("Unable to upload file: " + e.details)
+            throw e
+        }
+    }
+
+    fun downloadFile(realFileId: String): ByteArrayOutputStream {
+        try {
+            val outputStream = ByteArrayOutputStream()
+            service.files().get(realFileId)
+                .executeMediaAndDownloadTo(outputStream)
+            return outputStream
+        } catch (e: GoogleJsonResponseException) {
+            System.err.println("Unable to move file: " +e.details)
+            throw e
+        }
+    }
+
+}
