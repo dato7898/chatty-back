e19813ddef879a9e5441622bd585dfa1ec7a20b3
david <davidd7598@gmail.com>
2022-07-20 17:27:57 +0600

    test

--- src/main/kotlin/com/dato/chatty/Application.kt
+++ src/main/kotlin/com/dato/chatty/Application.kt
@@ -7,6 +7,7 @@ import org.springframework.core.task.SimpleAsyncTaskExecutor
 
 import org.springframework.core.task.TaskExecutor
 import org.springframework.scheduling.annotation.EnableAsync
+import java.io.File
 
 
 @EnableAsync
@@ -21,5 +22,6 @@ class Application {
 }
 
 fun main(args: Array<String>) {
+    println(File("./").absolutePath)
     runApplication<Application>(*args)
 }
