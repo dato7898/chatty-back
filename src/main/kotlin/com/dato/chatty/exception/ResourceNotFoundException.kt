69ec2a51a0afb1a98b95ff55fb1d5ac72865c3e1
dato7898 <davidd7598@gmail.com>
2022-07-24 13:03:53 +0600

    handled graphql exceptions

--- src/main/kotlin/com/dato/chatty/exception/ResourceNotFoundException.kt
+++ src/main/kotlin/com/dato/chatty/exception/ResourceNotFoundException.kt
@@ -6,4 +6,4 @@ import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 class ResourceNotFoundException(resourceName: String, fieldName: String, fieldValue: Any) :
-    RuntimeException(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue))
+    AbstractGraphQLException(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue))
