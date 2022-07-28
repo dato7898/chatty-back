b6faf888fe758cb3811272634ad730f6fe048128
david <davidd7598@gmail.com>
2022-07-28 16:23:52 +0600

    fix

--- src/main/kotlin/com/dato/chatty/repo/RoomRepo.kt
+++ src/main/kotlin/com/dato/chatty/repo/RoomRepo.kt
@@ -14,7 +14,8 @@ interface RoomRepo : JpaRepository<Room, Long> {
 
     fun findByIdAndUsersContains(roomId: Long, user: User): Optional<Room>
 
-    @Query("WITH max_date AS (SELECT MAX(created_at) createdAt, room_id FROM message GROUP BY room_id) " +
+    @Query("WITH max_date AS (SELECT MAX(created_at) createdAt, room_id FROM message m " +
+            "WHERE :userId NOT IN (SELECT md.deletes_id FROM message_deletes md WHERE m.id=md.message_id) GROUP BY room_id) " +
             "SELECT * FROM room r JOIN max_date md ON r.id = md.room_id " +
             "WHERE :userId IN (SELECT ru.users_id FROM room_users ru WHERE r.id=ru.room_id) " +
             "ORDER BY md.createdAt DESC", nativeQuery = true)
