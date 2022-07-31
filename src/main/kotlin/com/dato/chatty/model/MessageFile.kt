944c1ffae108b447fa6ddeee42e1dd43b0b102c7
dato7898 <davidd7598@gmail.com>
2022-07-31 20:48:40 +0600

    fixed Message File

--- src/main/kotlin/com/dato/chatty/model/MessageFile.kt
+++ src/main/kotlin/com/dato/chatty/model/MessageFile.kt
@@ -1,12 +1,13 @@
 package com.dato.chatty.model
 
+import java.io.Serializable
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-class MessageFile {
+class MessageFile : Serializable {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
